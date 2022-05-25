51347c05910ed5396d5013fa85e1eabddadec7fa
Jose Vega <Jose.Vega@gft.com>
2022-05-25 15:55:21 +0200

    fix winery remove extends of wine

--- src/main/java/com/example/wine/Classes/Winery.java
+++ src/main/java/com/example/wine/Classes/Winery.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Winery extends Wine {
+public class Winery {
     @Id
     @GeneratedValue
     private Long id;
@@ -17,31 +17,26 @@ public class Winery extends Wine {
         this.name = name;
     }
 
-    public Winery(String name, String year, float rating, int num_reviews, int price, int body, int acidity, String wineryName) {
-        super(name, year, rating, num_reviews, price, body, acidity);
-        this.name = wineryName;
-    }
-
     public Winery() {
 
     }
 
-    @Override
+
     public Long getId() {
         return id;
     }
 
-    @Override
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Override
+
     public String getName() {
         return name;
     }
 
-    @Override
+
     public void setName(String name) {
         this.name = name;
     }
