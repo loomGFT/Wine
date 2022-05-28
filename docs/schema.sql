53f21ba620c56bc9a44edcb9c38afe344732e2a0
Jose Vega <Jose.Vega@gft.com>
2022-05-28 14:18:17 +0200

    sql
    Signed-off-by: Jose Vega <Jose.Vega@gft.com>

---
+++ docs/schema.sql
@@ -0,0 +1,16 @@
+-- auto-generated definition
+create table wines_spa (
+    id INT not null
+        primary key,
+    winery TEXT null,
+    wine TEXT null,
+    year TEXT null,
+    rating DOUBLE null,
+    num_reviews INT null,
+    country TEXT null,
+    region TEXT null,
+    price DOUBLE null,
+    type TEXT null,
+    body TEXT null,
+    acidity TEXT null
+);
